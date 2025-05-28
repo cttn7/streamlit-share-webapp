d7c75084104a7ba2ac732fc17b942a3f5e760850
cttn7 <christotan07@gmail.com>
2025-05-28 15:57:03 +0800

    Trying to maximise map size (Iteration 7)

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ with tab1:
                                     mapbox_style="open-street-map",
                                     hover_name="cp-code",
                                     title="🔌 Nearby Charging Points",
-                                    height=700,  # Adjust height as needed
+                                    height=700  # Adjust height as needed
                                 )
 
                                 # Add reference point
